dc960e5bfbe0bb43e0289f674b70104e0d0581d4
LENOVO <mailmeinmymail0@gmail.com>
2021-05-10 22:12:26 +0530

    new layout

--- app/src/main/java/com/smartworld/backendlessapi/HomeActivity.java
+++ app/src/main/java/com/smartworld/backendlessapi/HomeActivity.java
@@ -2,13 +2,35 @@ package com.smartworld.backendlessapi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    Button btnList,btnCreate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        btnList = findViewById(R.id.btnList);
+        btnCreate = findViewById(R.id.btnCreate);
+
+        btnList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, ContactListActivity.class));
+            }
+        });
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, NewContactActivity.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/com/smartworld/backendlessapi/LoginActivity.java
+++ app/src/main/java/com/smartworld/backendlessapi/LoginActivity.java
@@ -14,6 +14,7 @@ import com.backendless.Backendless;
 import com.backendless.BackendlessUser;
 import com.backendless.async.callback.AsyncCallback;
 import com.backendless.exceptions.BackendlessFault;
+import com.backendless.persistence.local.UserIdStorageFactory;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -51,6 +52,7 @@ public class LoginActivity extends AppCompatActivity {
                     Backendless.UserService.login(email, password, new AsyncCallback<BackendlessUser>() {
                         @Override
                         public void handleResponse(BackendlessUser response) {
+                            ApplicationClass.user = response;
                             Toast.makeText(LoginActivity.this, "Logged in successfully!", Toast.LENGTH_SHORT).show();
                             startActivity(new Intent(LoginActivity.this, HomeActivity.class));
                             LoginActivity.this.finish();
@@ -89,6 +91,34 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+        Backendless.UserService.isValidLogin(new AsyncCallback<Boolean>() {
+            @Override
+            public void handleResponse(Boolean response) {
+                if (response){
+                    String userObjectId = UserIdStorageFactory.instance().getStorage().get();
 
+                    Backendless.Data.of(BackendlessUser.class).findById(userObjectId, new AsyncCallback<BackendlessUser>() {
+                        @Override
+                        public void handleResponse(BackendlessUser response) {
+                            ApplicationClass.user = response;
+                            startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                            LoginActivity.this.finish();
+                        }
+
+                        @Override
+                        public void handleFault(BackendlessFault fault) {
+                            Toast.makeText(LoginActivity.this, "Error: "+ fault.getMessage(), Toast.LENGTH_SHORT).show();
+//                                showProgress(false);
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void handleFault(BackendlessFault fault) {
+                Toast.makeText(LoginActivity.this, "Error: "+ fault.getMessage(), Toast.LENGTH_SHORT).show();
+//                                showProgress(false);
+            }
+        });
     }
 }
